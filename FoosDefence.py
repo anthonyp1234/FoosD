7270a0ccdb5f0a0cc6f34c69cd8f0fe9f0b59873
anthonyp1234 <anthony_p1234@yahoo.com>
2014-11-07 15:54:30 +1100

    updated Code

--- FoosDefence.py
+++ FoosDefence.py
@@ -16,31 +16,37 @@ from FoosDefence_class import *
 clock = pygame.time
 
 
-level_select = int(raw_input("Please select a level skill from 0 - 9, 0 is best, 9 is worst: ")) 
+level_select = int(raw_input("\n\nPlease select a level skill from 0 - 9, 0 is best, 9 is worst: ")) 
 
 if not level_select in range(0,9):
     print "Not in Range"
     sys.exit()
 
-level_select = int(raw_input("Please select a style from:\n 1) Snake, \n2)Pull\n: "))    
+type_select = int(raw_input("\nPlease select a style from:\n1) Snake, \n2) Pull\nCHOICE: "))    
 
-if not level_select in range(1,2):
+if not type_select in range(1,3):
     print "Not in Range"
     sys.exit()         
+
+if type_select == 1:    
+    player_position = snake(level_select) 
+else:
+    player_position = pull(level_select)
     
-player_position = position() 
 settings = setting()        
 
 while True:
     ### Do the stuff in here
-    
+    #import pdb; pdb.set_trace()
     ##Wait random amount of time
+    #print "\n\n############################New Round"
     time_value = settings.get_time_variance()
     waiting_time = random.gauss(time_value[0], time_value[1])
     #print waiting_time
     clock.delay(int(round(waiting_time)))
     #print "Move"
-    print player_position.weighted_choice(snake_percentages[level_select])
+    value = player_position.new_postitions()
+    print "{0}:{1}".format(value[0], value[1])
     
     
     
--- FoosDefence_class.py
+++ FoosDefence_class.py
@@ -1,39 +1,40 @@
 import random
 
-POSITIONS = 9  #  9 postitions if using half spaces. 5 is middle
 
 PREVIOUS = [0.1, 0.15, 0.2, 0.25]
 
 
 
 class position:
-    two_row = 6
-    goalie_row = 4   
-    two_row_previous = []
-    goalie_row_previous = []
-    
-    def check_crossing(self):
+    def __init__(self):
+        self.two_row = 6
+        self.goalie_row = 4   
+        self.two_row_previous = [6]
+        self.goalie_row_previous = [4]
+        self.percentage = [0.11, 0.11, 0.11,  0.11,  0.11, 0.11,  0.11, 0.11,  0.11]
+        self.cross_over_chance = 0.15  # Percentage chance to cross    
+        
+    def check_crossing(self, two_new, one_new):
         if self.two_row_previous[-1]:
             two_big_prev = (self.two_row_previous[-1] > self.goalie_row_previous[-1])  
-            one_big_prev = (self.two_row_previous[-1] < self.goalie_row_previous[-1])
-            two_big = self.two_row > self.goalie_row
-            one_big = self.goalie_row < self.two_row
+            two_big = two_new > one_new    
             
             if (two_big_prev and not two_big):
-                return true
+                return True
             elif (not two_big_prev and two_big):
-                return true
+                return True
             else:
-                return false
+                return False
         else:
-            return false
+            return False
             
     def select_postition(self):
         pass
-    
-    def weighted_choice(self,snake_percentages):
+        
+   
+    def weighted_choice(self):
         values = range(1,10) 
-        choices = zip(values, snake_percentages)
+        choices = zip(values, self.percentage)
         total = sum(w for c, w in choices)
         r = random.uniform(0, total)
         upto = 0
@@ -42,15 +43,19 @@ class position:
              return c
           upto += w
         assert False, "Shouldn't get here"
+        
+    
+        
+        
+        
      
     
 class setting:
-    defence_type = ""
     speedSelectChance = .80
     speed = [250, 500]  #speed in MS between steps. Median
     speed_variance = [50, 100]
     cover_previous = PREVIOUS  # Likelyhood to cover previously covered spot (x steps ago)
-    cross_over_chance = 0.15  # Percentage chance to cross
+
     
     def get_time_variance(self):
         change_time = []
@@ -66,22 +71,104 @@ class setting:
     
     
 
-class snake(setting):
+class snake(position):
     
-    snake_percentages =  [[0.11, 0.11, 0.11,  0.11,  0.11, 0.11,  0.11, 0.11,  0.11],
-                          [0.09, 0.11, 0.11,  0.12,  0.13, 0.12,  0.11, 0.11,  0.09],
-                          [0.07, 0.11, 0.11,  0.13,  0.15, 0.13,  0.11, 0.11,  0.07],
-                          [0.05, 0.11, 0.12,  0.14,  0.15, 0.14,  0.12, 0.11,  0.05],
-                          [0.05, 0.08, 0.145, 0.15,  0.15, 0.15,  0.145, 0.08, 0.05],
-                          [0.04, 0.06, 0.15,  0.16,  0.17, 0.16,  0.15, 0.06,  0.04],
-                          [0.03, 0.03, 0.17,  0.175, 0.18, 0.175, 0.17, 0.03,  0.03],
-                          [0.02, 0.02, 0.18,  0.18,  0.19, 0.18,  0.18, 0.02,  0.02],
-                          [0.01, 0.01, 0.185, 0.19,  0.2,  0.19,  0.185, 0.01, 0.01],
-                          [0,       0, 0.2,   0.2,   0.2,  0.2,   0.2,   0,    0   ]
-                         ]
+    def __init__(self, level):
+        position.__init__(self)
+        self.level = level
+        self.percentages =  [[0.11, 0.11, 0.11,  0.11,  0.11, 0.11,  0.11, 0.11,  0.11],
+                        [0.09, 0.11, 0.11,  0.12,  0.13, 0.12,  0.11, 0.11,  0.09],
+                        [0.07, 0.11, 0.11,  0.13,  0.15, 0.13,  0.11, 0.11,  0.07],
+                        [0.05, 0.11, 0.12,  0.14,  0.15, 0.14,  0.12, 0.11,  0.05],
+                        [0.05, 0.08, 0.145, 0.15,  0.15, 0.15,  0.145, 0.08, 0.05],
+                        [0.04, 0.06, 0.15,  0.16,  0.17, 0.16,  0.15, 0.06,  0.04],
+                        [0.03, 0.03, 0.17,  0.175, 0.18, 0.175, 0.17, 0.03,  0.03],
+                        [0.02, 0.02, 0.18,  0.18,  0.19, 0.18,  0.18, 0.02,  0.02],
+                        [0.01, 0.01, 0.185, 0.19,  0.2,  0.19,  0.185, 0.01, 0.01],
+                        [0,       0, 0.2,   0.2,   0.2,  0.2,   0.2,   0,    0   ]
+                        ]
+        self.percentage = self.percentages[self.level]
     
-    median = (POSITIONS)/2
-    stddev = ""
+    def new_postitions(self):
+        position1 = self.weighted_choice()
+        position2 = self.weighted_choice()
+        #print "Currently: " + str(position1) + " and " + str(position2) 
+        if self.check_crossing(position1, position2):
+            if random.random() < self.cross_over_chance:
+                #print "\nFirst if:"
+                #print str(position1) + " and " + str(position2)
+                return (position1, position2)
+            else:
+                #print "\nIn Else clause:" 
+                return self.new_postitions()
+        else:
+            return (position1, position2)
+            #print "\nLast ELSE:"
+            #print str(position1) + " and " + str(position2)
+
+    def two_space_chance(self):
+        if self.level >= 8:
+            return False
+        else:
+            if random.random() < ((10 - self.level)/10):
+                return True
+            else:
+                return False
+    
+
+                         
+class pull(position):
+    
+    def __init__(self, level):
+        position.__init__(self)
+        self.level = level
+
+        self.percentages = [[0, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125],
+                            [0.125, 0.14, 0.125, 0.125, 0.125, 0.125, 0.125, 0.11, 0],
+                            [0.125, 0.15, 0.145, 0.14, 0.14, 0.125, 0.125, 0.05, 0],
+                            [0.1, 0.2, 0.17, 0.17, 0.16, 0.125, 0.05, 0.025, 0],
+                            [0.12, 0.2, 0.2, 0.2, 0.145, 0.1, 0.025, 0.01, 0],
+                            [0.175, 0.2, 0.2, 0.2, 0.13, 0.07, 0.02, 0.005, 0],
+                            [0.2, 0.21, 0.21, 0.21, 0.11, 0.05, 0.01, 0, 0],
+                            [0.2, 0.25, 0.21, 0.215, 0.1, 0.025, 0, 0, 0],
+                            [0.21, 0.25, 0.25, 0.23, 0.05, 0.0125, 0, 0, 0],
+                            [0.19, 0.3, 0.2, 0.3, 0.01, 0, 0, 0, 0]
+                           ]
+        self.percentage = self.percentages[self.level]
+    
+    def new_postitions(self):
+        position1 = self.weighted_choice()
+        position2 = self.weighted_choice()
+        
+        ### Check to make sure not same (and greater the half space):
+        if abs(position1 - position2) <= self.two_space_chance():
+            return self.new_postitions()
+        
+        
+        #print "Currently: " + str(position1) + " and " + str(position2) 
+        if self.check_crossing(position1, position2):
+            if random.random() < self.cross_over_chance:
+                #print "\nFirst if:"
+                #print str(position1) + " and " + str(position2)
+                return (position1, position2)
+            else:
+                #print "\nIn Else clause:" 
+                return self.new_postitions()
+        else:
+            return (position1, position2)
+            #print "\nLast ELSE:"
+            #print str(position1) + " and " + str(position2)    
+            
+            
+    def two_space_chance(self):
+        if self.level >= 8:
+            return 1
+        else:
+            if random.random() < ((10 - self.level)/10):
+                return 2
+            else:
+                return 1
+
     
     
     
